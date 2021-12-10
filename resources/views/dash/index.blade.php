b720a267a5287635d75b2bb2e0afb9e0ed8a34a6
iruzts <78693921+iruzts@users.noreply.github.com>
2021-12-10 14:41:19 -0600

    correccion

--- resources/views/dash/index.blade.php
+++ resources/views/dash/index.blade.php
@@ -126,7 +126,7 @@
                                 @csrf
                                 @method('PUT')
                                 <input type="hidden" name="estado" value="Entregado">
-                                <input type="hidden" name="estado" value="Pagado">
+                                <input type="hidden" name="Pagado" value="Pagado">
                                 <button type="submit" class="btn btn-success btn-sm mx-1">
                                     Entregar</i>
                                 </button>
